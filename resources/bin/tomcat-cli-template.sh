203e9c4a024be2a60bca8c3c5cff84c7390e87f7
Pidster <pid@pidster.com>
2010-09-04 15:23:17 +0100

    Minor changes to shell script

--- resources/bin/tomcat-cli-template.sh
+++ resources/bin/tomcat-cli-template.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
-#
-#
+# 
+# Sample shell script wrapper for Tomcat-CLI
+# 
 # --------------------------------------------------------
 
-SPATH="$_"
-DPATH=`/usr/bin/dirname "$SPATH"`
-
 JAVA_BIN=""
-JAVA_TOOLS=""
+JAVA_TOOLS="."
 
 if [ -e $JAVA_HOME/bin/java ]; then
     JAVA_BIN="$JAVA_HOME/bin/java"    
-    JAVA_TOOLS="$JAVA_HOME/lib/tools.jar"    
+    JAVA_TOOLS="$JAVA_HOME/lib/tools.jar"
 elif [ -e $JRE_HOME/bin/java ]; then
     JAVA_BIN="$JRE_HOME/bin/java"
-    JAVA_TOOLS="$JRE_HOME/lib/tools.jar"    
+    JAVA_TOOLS="$JRE_HOME/lib/tools.jar"
 else
     JAVA_BIN=`/usr/bin/whereis java`
 fi
 
 if [ ! -e $JAVA_BIN ]; then
-    echo "Couldn't find java, set JAVA_HOME or JRE_HOME"
+    echo "Couldn't find java, please set JAVA_HOME or JRE_HOME"
     exit 1
 fi
 
@@ -29,4 +27,7 @@ if [ ! -e "$DPATH/@appname.jar" ]; then
     exit 1
 fi
 
-$JAVA_BIN -cp "$JAVA_TOOLS:$DPATH/@appname.jar" org.pidster.tomcat.util.cli.Console $@
+SPATH="$_"
+DPATH=`/usr/bin/dirname "$SPATH"`
+
+$JAVA_BIN -cp "$DPATH/@appname.jar:$JAVA_TOOLS" org.pidster.tomcat.util.cli.Console $@
