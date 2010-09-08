1dc6508fb46d1ee253aefeb4cf1ec6ae97f67dab
Pidster <pid@pidster.com>
2010-09-08 09:46:48 +0100

    Fix template script

--- resources/bin/tomcat-cli-template.sh
+++ resources/bin/tomcat-cli-template.sh
@@ -7,27 +7,27 @@
 JAVA_BIN=""
 JAVA_TOOLS="."
 
-if [ -e $JAVA_HOME/bin/java ]; then
+if [ -f $JAVA_HOME/bin/java ]; then
     JAVA_BIN="$JAVA_HOME/bin/java"    
     JAVA_TOOLS="$JAVA_HOME/lib/tools.jar"
-elif [ -e $JRE_HOME/bin/java ]; then
+elif [ -f $JRE_HOME/bin/java ]; then
     JAVA_BIN="$JRE_HOME/bin/java"
     JAVA_TOOLS="$JRE_HOME/lib/tools.jar"
 else
     JAVA_BIN=`/usr/bin/whereis java`
 fi
 
-if [ ! -e $JAVA_BIN ]; then
+if [ ! -f $JAVA_BIN ]; then
     echo "Couldn't find java, please set JAVA_HOME or JRE_HOME"
-    exit 1
-fi
-
-if [ ! -e "$DPATH/@appname.jar" ]; then
-    echo "Couldn't find jar file, '$DPATH/@appname.jar'"
     exit 1
 fi
 
 SPATH="$_"
 DPATH=`/usr/bin/dirname "$SPATH"`
+
+if [ ! -f "$DPATH/@appname.jar" ]; then
+    echo "Couldn't find jar file, '$DPATH/@appname.jar'"
+    exit 1
+fi
 
 $JAVA_BIN -cp "$DPATH/@appname.jar:$JAVA_TOOLS" org.pidster.tomcat.util.cli.Console $@
